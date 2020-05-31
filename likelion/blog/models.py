b7484a60f868701fa147235e47f04209585a0783
hkh0105 < ! hkh9601@naver.com>
2020-05-31 20:39:37 +0900

    과제확인

---
+++ likelion/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateField()
+    body = models.TextField()
+    image = models.ImageField(upload_to="blog/")
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
--- likelion/likelion/urls.py
+++ likelion/likelion/urls.py
@@ -1,25 +1,26 @@
-"""likelion URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 import workout.views
+import blog.views
+import account.urls
+import account.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',workout.views.home, name = "home"),
+    path('wordcoung',workout.views.home, name = "home"),
     path('about',workout.views.about, name = "about"),
     path('result/',workout.views.result, name = "result"),
-]
+   
+    path('account/', include(account.urls)), 
+    path('',blog.views.list, name = "list"),
+    path('blog/<int:blog_id>',blog.views.detail, name = "detail"),
+    path('blog/new',blog.views.new, name = "new"),
+    path('blog/create',blog.views.create, name = "create"),
+    path('blog/edit/<int:blog_id>',blog.views.edit, name = "edit"),
+    path('blog/update/<int:blog_id>',blog.views.update, name = "update"),
+    path('blog/delete/<int:blog_id>',blog.views.delete, name = "delete"),
+    path('account/register', account.views.register_view,name="register"),
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
